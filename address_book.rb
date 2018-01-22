24193831a87c3154cf85e299b76e2e93c976472c
stacietaylorcima <stacietaylorcima@gmail.com>
2018-01-22 13:54:46 -0500

    create MenuController and add the following functionality: handle user input, create new entry, view entries

--- address_book.rb
+++ address_book.rb
@@ -1,1 +1,8 @@
-puts "Welcome to your Address Book!"
+require_relative 'controllers/menu_controller'
+
+	menu = MenuController.new
+
+	system "clear"
+	puts "Welcome to your Address Book!"
+
+	menu.main_menu
